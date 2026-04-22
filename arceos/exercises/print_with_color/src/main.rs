153ce13e469d4753707ba0dc511bc8df0bf00681
wiesn2333 <wei_shu_ning@outlook.com>
2026-04-22 20:52:44 +0800

    Add color to print_with_color output

--- arceos/exercises/print_with_color/src/main.rs
+++ arceos/exercises/print_with_color/src/main.rs
@@ -6,5 +6,5 @@ use axstd::println;
 
 #[cfg_attr(feature = "axstd", no_mangle)]
 fn main() {
-    println!("[WithColor]: Hello, Arceos!");
+    println!("[WithColor]: \x1b[31mHello, Arceos!\x1b[0m");
 }
