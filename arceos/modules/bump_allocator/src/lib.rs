1c194c3906b6815cc2b54d8e398b1e45b52598d1
wiesn2333 <wei_shu_ning@outlook.com>
2026-04-24 10:39:03 +0800

    Implement bump allocation

--- arceos/modules/bump_allocator/src/lib.rs
+++ arceos/modules/bump_allocator/src/lib.rs
@@ -16,17 +16,43 @@ use allocator::{BaseAllocator, ByteAllocator, PageAllocator};
 /// When it goes down to ZERO, free bytes-used area.
 /// For pages area, it will never be freed!
 ///
-pub struct EarlyAllocator<const SIZE: usize> {}
+pub struct EarlyAllocator<const SIZE: usize> {
+    start_vaddr: usize,
+    size: usize,
+    b_pos: usize,
+    p_pos: usize,
+    count: usize,
+    pages_allocated: usize,
+}
+
+const fn align_down(pos: usize, align: usize) -> usize {
+    pos & !(align - 1)
+}
+const fn align_up(pos: usize, align: usize) -> usize {
+    (pos + align - 1) & !(align - 1)
+}
 
 impl<const SIZE: usize> EarlyAllocator<SIZE> {
     pub const fn new() -> Self {
-        Self {}
+        Self {
+            start_vaddr: 0,
+            size: 0,
+            b_pos: 0,
+            p_pos: 0,
+            count: 0,
+            pages_allocated: 0,
+        }
     }
 }
 
 impl<const SIZE: usize> BaseAllocator for EarlyAllocator<SIZE> {
     fn init(&mut self, start: usize, size: usize) {
-        todo!()
+        self.start_vaddr = start;
+        self.size = size;
+        self.b_pos = start;
+        self.p_pos = start + size;
+        self.count = 0;
+        self.pages_allocated = 0;
     }
 
     fn add_memory(&mut self, start: usize, size: usize) -> allocator::AllocResult {
@@ -34,16 +60,55 @@ impl<const SIZE: usize> BaseAllocator for EarlyAllocator<SIZE> {
     }
 }
 
+struct Node {
+    size: usize,
+    align: usize,
+    prev: usize,
+    next: usize,
+}
+const NODE_SIZE: usize = core::mem::size_of::<Node>();
+
 impl<const SIZE: usize> ByteAllocator for EarlyAllocator<SIZE> {
     fn alloc(
         &mut self,
         layout: core::alloc::Layout,
     ) -> allocator::AllocResult<core::ptr::NonNull<u8>> {
-        todo!()
+        let node_addr = align_up(self.b_pos, layout.align());
+        let addr = align_up(node_addr + NODE_SIZE, layout.align());
+        if addr + layout.size() > self.p_pos {
+            return allocator::AllocResult::Err(allocator::AllocError::NoMemory);
+        }
+        let node = unsafe { &mut *(node_addr as *mut Node) };
+        node.size = layout.size();
+        node.align = layout.align();
+        node.prev = self.b_pos;
+        node.next = 0;
+
+        self.b_pos = addr + layout.size() + NODE_SIZE;
+        self.count += 1;
+        allocator::AllocResult::Ok(core::ptr::NonNull::new(addr as *mut u8).unwrap())
     }
 
     fn dealloc(&mut self, pos: core::ptr::NonNull<u8>, layout: core::alloc::Layout) {
-        todo!()
+        let node_addr = align_down(pos.as_ptr() as usize - NODE_SIZE, layout.align());
+        let node = unsafe { &mut *(node_addr as *mut Node) };
+        if node.size != layout.size() {
+            panic!("dealloc size mismatch");
+        }
+        if node.next == 0 {
+            let prev_node = unsafe { &mut *(node.prev as *mut Node) };
+            prev_node.next = 0;
+            self.b_pos = align_up(node.prev + NODE_SIZE, node.align) + node.size;
+        }else{
+            let next_node = unsafe { &mut *(node.next as *mut Node) };
+            next_node.prev = node.prev;
+            if node_addr != align_up(self.start_vaddr, node.align) {
+                let prev_node = unsafe { &mut *(node.prev as *mut Node) };
+                prev_node.next = node.next;
+            }
+            self.b_pos = align_up(node_addr + NODE_SIZE, node.align) + node.size;
+        }
+        self.count -= 1;
     }
 
     fn total_bytes(&self) -> usize {
